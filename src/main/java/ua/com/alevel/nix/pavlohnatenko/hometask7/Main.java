08732cabbe1a0d2daa7763f674283b697fdaf9cd
Pavlo-Hnatenko <gnatenko.pavlo@gmail.com>
2020-07-03 08:05:39 +0300

    fourth commit: refactored class "Main"

--- src/main/java/ua/com/alevel/nix/pavlohnatenko/hometask7/Main.java
+++ src/main/java/ua/com/alevel/nix/pavlohnatenko/hometask7/Main.java
@@ -3,26 +3,27 @@ package ua.com.alevel.nix.pavlohnatenko.hometask7;
 import ua.com.alevel.nix.pavlohnatenko.hometask7.data.*;
 import ua.com.alevel.nix.pavlohnatenko.hometask7.service.PlanetReporter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        AbstractPlanet mercury = Mercury.getInstance();
-        PlanetReporter.printGravityAcceleration(mercury);
-        AbstractPlanet venus = Venus.getInstance();
-        PlanetReporter.printGravityAcceleration(venus);
-        AbstractPlanet earth = Earth.getInstance();
-        PlanetReporter.printGravityAcceleration(earth);
-        AbstractPlanet mars = Mars.getInstance();
-        PlanetReporter.printGravityAcceleration(mars);
-        AbstractPlanet jupiter = Jupiter.getInstance();
-        PlanetReporter.printGravityAcceleration(jupiter);
-        AbstractPlanet saturn = Saturn.getInstance();
-        PlanetReporter.printGravityAcceleration(saturn);
-        AbstractPlanet uranus = Uranus.getInstance();
-        PlanetReporter.printGravityAcceleration(uranus);
-        AbstractPlanet neptune = Neptune.getInstance();
-        PlanetReporter.printGravityAcceleration(neptune);
+        List<AbstractPlanet> solarSystem = new ArrayList<>();
+        solarSystem.add(Mercury.getInstance());
+        solarSystem.add(Venus.getInstance());
+        solarSystem.add(Earth.getInstance());
+        solarSystem.add(Mars.getInstance());
+        solarSystem.add(Jupiter.getInstance());
+        solarSystem.add(Saturn.getInstance());
+        solarSystem.add(Uranus.getInstance());
+        solarSystem.add(Neptune.getInstance());
+
+        for (AbstractPlanet planet:
+             solarSystem) {
+            PlanetReporter.printGravityAcceleration(planet);
+        }
 
     }
 }
